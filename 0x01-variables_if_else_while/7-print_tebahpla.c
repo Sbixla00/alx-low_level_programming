e1803b70897ca6f81f4098be58a953a4f1532148
sbixla00 <ayacheayoub25@gmail.com>
2023-09-08 09:43:47 -0700

    ll

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Descreption : A C program that prints with put function
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	int i = 0;
+
+	for (i = 122; i > 96; i--)
+	{
+	putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
