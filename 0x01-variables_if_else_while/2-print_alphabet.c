db2f00269d6c76604b95d81d04f7dafd1c87c590
sbixla00 <ayacheayoub25@gmail.com>
2023-09-08 08:01:57 -0700

    ll

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Descreption : A C program that prints with put function
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	for (int  i = 97; i < 123; i++)
+	{
+	putchar(i);
+	}
+	return (0);
+}
