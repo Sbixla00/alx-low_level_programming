2c1649c304dae976cbdea1bfed7bebf0c18a57e6
sbixla00 <ayacheayoub25@gmail.com>
2023-09-13 16:46:04 -0700

    ll

--- 0x02-functions_nested_loops/alphaber.c
+++ 0x02-functions_nested_loops/alphaber.c
@@ -5,13 +5,13 @@
  * Return:0
 */
 
-void print_alphabet(void)
+int main(void)
 {
-	int i;
+	int j;
 
-	for (j = 97; i <= 122; i++)
+	for (j = 97; j <= 122; j++)
 	{
-		_putchar(i);
+		_putchar(j);
 	}
 	_putchar('\n');
 
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,2 @@
-#ifdef MAIN_h
-#define MAINE_H
-
-#include <stdio.h>
 int _putchar(char c);
 void print_alphabet(void);
-
-
-#endif
