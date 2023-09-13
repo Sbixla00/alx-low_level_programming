3f96d98039185fab4dd6f1b082ed60857507b4d1
sbixla00 <ayacheayoub25@gmail.com>
2023-09-13 16:57:29 -0700

    ll

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,7 +3,9 @@
  * print_alphabet - Entry point
  * Return:0
  */
-void print_alphabet(void)
+void print_alphabet_x10(void)
+{
+for(int i = 0; i < 10; i++)
 {
 char c = 'a';
 while (c <= 'z')
@@ -12,4 +14,7 @@ _putchar(c);
 c++;
 }
 _putchar('\n');
+}
+}
+
 
