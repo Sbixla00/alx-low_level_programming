682a29811e91cf313667a63c2c61d186c66f0189
sbixla00 <ayacheayoub25@gmail.com>
2023-09-18 09:22:22 -0700

    ll

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ **_strcpy - copies string pointed to by src
+ *@dest:destination
+ *@src:source
+ *
+ *Return:pointer to dest
+ *
+ */
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+}
+
