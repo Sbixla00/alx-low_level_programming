d9258c6761765f4f1e70b773964015ebf01b003c
sbixla00 <ayacheayoub25@gmail.com>
2023-10-24 10:13:48 -0700

    ll

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -17,5 +17,15 @@ typedef struct listint_s
 	        struct listint_s *next;
 } listint_t;
 size_t print_listint(const listint_t *h);
+int _putchar(char c);
+size_t listint_len(const listint_t *h);
+
+
+
+
+
+
+
+
 
 #endif
