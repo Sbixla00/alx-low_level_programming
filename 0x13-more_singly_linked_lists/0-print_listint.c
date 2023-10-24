706884625c9a22577a159e21ba367c144d6dc88b
sbixla00 <ayacheayoub25@gmail.com>
2023-10-24 09:36:13 -0700

    ll

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ *  *print_listint - prints all elements of a print_listint list
+ *   *@h:pointer to a listint struct
+ *    *Return:no. of nodes
+*/
+size_t print_listint(const listint_t *h)
+{
+	size_t n;
+iize_t print_list(const list_t *h)
+
+	for (n = 0; h->next; n++)
+	{
+		printf("%d", h->n);
+		h = h->next;
+	}
+	return (n);
+}
